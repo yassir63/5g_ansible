bcf722689b2e68e24e6bc43f0d7578a2ad982f48
ZIYAD MABROUK <mabrouk.ziyad@inemail.ine.inpt.ma>
2025-07-01 02:30:58 +0200

    add new test scenarios

--- run_iperf_test.sh
+++ run_iperf_test.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+# Install required Ansible collections
 ansible-galaxy install -r collections/requirements.yml
 
+# Default playbook
 PLAYBOOK="playbooks/run_iperf_test.yml"
-while getopts ":p" opt; do
+
+# Parse options
+while getopts ":pd" opt; do
   case ${opt} in
     p )
       PLAYBOOK="playbooks/run_parallel_iperf_test.yml"
       ;;
+    d )
+      PLAYBOOK="playbooks/run_default_test.yml"
+      ;;
     \? )
-      echo "Usage: $0 [-p]"
+      echo "Usage: $0 [-p] [-d]"
+      echo "  -p  Run parallel iperf test playbook"
+      echo "  -d  Run default test playbook"
       exit 1
       ;;
   esac
 done
 
+# Run the selected playbook
 ansible-playbook -i inventory/hosts.ini "$PLAYBOOK"
