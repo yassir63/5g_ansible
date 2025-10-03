a92aba865ee646039287f3b2e85809e6b39ace39
turletti <turletti@users.noreply.github.com>
2025-10-03 23:35:19 +0200

    fix

--- playbooks/start_block2.sh
+++ playbooks/start_block2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Full absolute paths
 ANSIBLE_PLAYBOOK="/usr/bin/ansible-playbook"
 INVENTORY="/home/turletti/xp/post5g-beta/ptp_test/5g_ansible/playbooks/inventory/hosts-test.ini"
 BLOCK2_PLAYBOOK="/home/turletti/xp/post5g-beta/ptp_test/5g_ansible/playbooks/block2_r2lab.yml"
 LOG_FILE="/home/turletti/xp/post5g-beta/ptp_test/5g_ansible/playbooks/block2.log"
 
-# Launch Block2 in fully detached background
-nohup $ANSIBLE_PLAYBOOK -i $INVENTORY $BLOCK2_PLAYBOOK -c local > $LOG_FILE 2>&1 &
+# Launch Block2 fully detached
+setsid $ANSIBLE_PLAYBOOK -i $INVENTORY $BLOCK2_PLAYBOOK -c local > $LOG_FILE 2>&1 < /dev/null &
+
