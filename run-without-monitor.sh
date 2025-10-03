4cc6fd2061c69e708822ccf0528ebf35d3c2def3
turletti <turletti@users.noreply.github.com>
2025-10-03 21:09:31 +0200

    speed up the scenario

--- run-without-monitor.sh
+++ run-without-monitor.sh
@@ -1,2 +1,2 @@
 ansible-galaxy install -r collections/requirements.yml
-time ansible-playbook -i inventory/hosts-test.ini playbooks/deploy_benetel-test.yml 2>&1 | tee logs-run.txt
+time ansible-playbook -i inventory/hosts-test.ini playbooks/deploy_benetel-without_monitor.yml 2>&1 | tee logs-run.txt
