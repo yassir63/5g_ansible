ca0e94830b7cb5d26bd49da02cb31dbc5cf5d21a
turletti <turletti@users.noreply.github.com>
2025-10-04 10:39:08 +0200

    fix

--- run-without-monitor.sh
+++ run-without-monitor.sh
@@ -1,3 +1,5 @@
 ansible-galaxy install -r collections/requirements.yml
-time ansible-playbook -i inventory/hosts-test.ini playbooks/deploy_benetel-without-r2lab.yml 2>&1 | tee logs-run.txt &
-time ansible-playbook -i inventory/hosts-test.ini playbooks/deploy_r2lab.yml 2>&1 | tee logs-r2lab.txt &
+( ansible-playbook -i inventory/hosts-test.ini playbooks/deploy_benetel-without-r2lab.yml 2>&1 | tee logs-run.txt ) &
+pid=$!; echo "**** PID of main script: $pid"
+( ansible-playbook -i inventory/hosts-test.ini playbooks/deploy_r2lab.yml 2>&1 | tee logs-r2lab.txt ) &
+pid=$!; echo "**** PID of R2lab setup script: $pid"
