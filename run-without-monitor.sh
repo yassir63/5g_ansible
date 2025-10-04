5dd8b61de20e1201abd71c9a2c88d1666f1385b1
turletti <turletti@users.noreply.github.com>
2025-10-04 10:28:17 +0200

    split the deployment playbook in 2 to speed up the scenario

--- run-without-monitor.sh
+++ run-without-monitor.sh
@@ -1,2 +1,3 @@
 ansible-galaxy install -r collections/requirements.yml
-time ansible-playbook --force -i inventory/hosts-test.ini playbooks/deploy_benetel-without_monitor.yml 2>&1 | tee logs-run.txt
+time ansible-playbook -i inventory/hosts-test.ini playbooks/deploy_benetel-without-r2lab.yml 2>&1 | tee logs-run.txt &
+time ansible-playbook -i inventory/hosts-test.ini playbooks/deploy_r2lab.yml 2>&1 | tee logs-r2lab.txt &
