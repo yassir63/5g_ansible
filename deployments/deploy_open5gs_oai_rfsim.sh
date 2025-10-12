4754ba0fc10dfec0453ef74008fa9f43606246ba
mbtavasoli <78649156+mbtavasoli@users.noreply.github.com>
2025-10-12 11:12:56 +0200

    exporting RRU value

--- deployments/deploy_open5gs_oai_rfsim.sh
+++ deployments/deploy_open5gs_oai_rfsim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-galaxy install -r collections/requirements.yml
-ansible-playbook -i inventory/hosts.ini playbooks/deploy_open5gs_oai_rfsim.yml
+ansible-playbook -i inventory/hosts.ini playbooks/deploy_open5gs_oai_rfsim.yml -e "rru=$RRU"
