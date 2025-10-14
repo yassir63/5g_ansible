b50336f54c6df55c470a9b151afa0402d5128d8c
Mohammad <mb.tavasoli@yahoo.com>
2025-10-14 16:08:09 +0200

    All deploy scripts updated

--- deployments/deploy_oai_r2lab.sh
+++ deployments/deploy_oai_r2lab.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ansible-galaxy install -r collections/requirements.yml
-ansible-playbook -i inventory/hosts.ini playbooks/deploy_oai_r2lab.yml -e "rru=$RRU"
+ansible-playbook -i inventory/hosts.ini playbooks/deploy_oai_r2lab.yml  -e "rru=$RRU 
