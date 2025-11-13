9a7f20763fc84f7c12adde01edf4bea25300a256
ZIYAD MABROUK <mabrouk.ziyad@inemail.ine.inpt.ma>
2025-11-14 00:12:27 +0100

    add no-setup option to run_interfernce_test.sh

--- scenarios/run_interference_test.sh
+++ scenarios/run_interference_test.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
 ansible-galaxy install -r collections/requirements.yml
-ansible-playbook -i inventory/hosts.ini playbooks/interference_test_setup.yml
+
+# By default, run setup
+RUN_SETUP=true
+
+# First handle long options
+new_args=()
+for arg in "$@"; do
+  case "$arg" in
+    --no-setup)
+      RUN_SETUP=false
+      ;;
+    --*)
+      echo "Unknown option: $arg"
+      echo "Usage: $0 [--no-setup]"
+      exit 1
+      ;;
+    *)
+      new_args+=("$arg")
+      ;;
+  esac
+done
+set -- "${new_args[@]}"
+
+if $RUN_SETUP; then
+    ansible-playbook -i inventory/hosts.ini playbooks/interference_test_setup.yml
+fi
 
 if [[ "$MODE" == "TDD" ]]; then
     ansible-playbook -i inventory/hosts.ini playbooks/run_interference_test_tdd.yml
