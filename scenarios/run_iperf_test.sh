2978a100a31bec3bd52d9be666970c64bd06c2b9
ZIYAD MABROUK <mabrouk.ziyad@inemail.ine.inpt.ma>
2025-11-25 21:23:38 +0100

    major updates

--- scenarios/run_iperf_test.sh
+++ scenarios/run_iperf_test.sh
@@ -20,29 +20,43 @@ for arg in "$@"; do
       ;;
     --*)
       echo "Unknown option: $arg"
-      echo "Usage: $0 [-d] [-p] [-i] [--no-setup]"
+      echo "Usage: $0 [-s] [-d] [-p] [-i] [--no-setup]"
       exit 1
       ;;
   esac
 done
 
 # Now parse short options
+# For iperf tests without interference, we will not use a setup playbook that connects UEs to the network, 
+# since they should already be connected by the deploy playbook.
 while getopts ":dpi" opt; do
   case ${opt} in
+    s )
+      RUN_SETUP=false
+      TEST_PLAYBOOK="playbooks/run_rfsim_iperf_test.yml"
+      ;;
     d )
-      SETUP_PLAYBOOK="playbooks/default_iperf_test_setup.yml"
+      RUN_SETUP=false
       TEST_PLAYBOOK="playbooks/run_default_iperf_test.yml"
       ;;
     p )
-      SETUP_PLAYBOOK="playbooks/parallel_iperf_test_setup.yml"
+      RUN_SETUP=false
       TEST_PLAYBOOK="playbooks/run_parallel_iperf_test.yml"
       ;;
     i )
       SETUP_PLAYBOOK="playbooks/interference_test_setup.yml"
-      TEST_PLAYBOOK="playbooks/run_interference_test.yml"
+      if [[ "$MODE" == "TDD" ]]; then
+          TEST_PLAYBOOK="playbooks/run_interference_test_tdd.yml"
+      elif [[ "$MODE" == "FDD" ]]; then
+          TEST_PLAYBOOK="playbooks/run_interference_test_fdd.yml"
+      else
+          echo "Error: MODE must be set to either 'TDD' or 'FDD' as environment variable for interference test."
+          exit 1
+      fi
       ;;
     \? )
       echo "Usage: $0 [-d] [-p] [-i] [--no-setup]"
+      echo "  -s           Use OAI rfsim iperf test playbook"
       echo "  -d           Use default iperf test playbook"
       echo "  -p           Use parallel iperf test playbook"
       echo "  -i           Use interference iperf test playbook"
