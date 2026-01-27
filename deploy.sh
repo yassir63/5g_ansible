8214ff15976deb06f73720946c403820033a4e74
Thierry Turletti <turletti@users.noreply.github.com>
2026-01-27 12:14:03 +0100

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -784,10 +784,10 @@ ansible-galaxy install -r collections/requirements.yml
 ##temporary disable r2lab playbook
 if [[ "$platform" == "r2lab" ]]; then
     echo "ansible-playbook -i "$INVENTORY" playbooks/deploy_r2lab.yml &"
-    #ansible-playbook -i "$INVENTORY" playbooks/deploy_r2lab.yml 2>&1 | tee logs-r2lab.txt &
+    ansible-playbook -i "$INVENTORY" playbooks/deploy_r2lab.yml 2>&1 | tee logs-r2lab.txt &
 fi
 echo "ansible-playbook -i "$INVENTORY" playbooks/deploy.yml"
-#ansible-playbook -i "$INVENTORY" playbooks/deploy.yml 2>&1 | tee logs.txt
+ansible-playbook -i "$INVENTORY" playbooks/deploy.yml 2>&1 | tee logs.txt
 
 echo ""
 echo "=========================================="
